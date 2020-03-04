bf8b9dc6283244888862fe96d32012dde6c6754e
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-03-04 00:10:45 -0800

    Comment out tests in test_videostacker.py until source files are added.

--- tests/test_videostacker.py
+++ tests/test_videostacker.py
@@ -12,42 +12,41 @@ class TestVideoStacker(unittest.TestCase):
         return os.path.normpath(path_one) == os.path.normpath(path_two)
 
     def test_videosplit(self):
-        return None
-        vs.split_video("./imgvid_utils/test_files/00000.MTS", "temp/", "kitty", "png", frame_count=100, start_frame=15)
-        self.assertEqual(100, len(fo.get_files("./temp/", "png")))
-        files = fo.get_files("./temp", "png")
-        for i in range(100):
-            self.assertTrue(self.assertFilePathEqual("./temp/kitty%s.png" % str(i).zfill(99), files[i]))
-
-        fo.clear_files("./temp/", "png")
+        # vs.split_video("./imgvid_utils/test_files/00000.MTS", "temp/", "kitty", "png", frame_count=100, start_frame=15)
+        # self.assertEqual(100, len(fo.get_files("./temp/", "png")))
+        # files = fo.get_files("./temp", "png")
+        # for i in range(100):
+        #     self.assertTrue(self.assertFilePathEqual("./temp/kitty%s.png" % str(i).zfill(99), files[i]))
+        #
+        # fo.clear_files("./temp/", "png")
+        pass
 
     def test_video_from_videos(self):
-        return None
-        vs.make_video_from_videos(["./imgvid_utils/test_files/00000.MTS", "./imgvid_utils/test_files/00000.MTS"]
-                                  , "temp/", "kitty", "mp4", cols=2, rows=1, width=800, height=600)
-
-        video = cv2.VideoCapture("./imgvid_utils/test_files/00000.MTS")
-        fps = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        ret = True
-        while ret:
-            ret, frame = video.read()
-        video.release()
-        vid = cv2.VideoCapture("./temp/kitty.mp4")
-        width_vid = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height_vid = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps_vid = vid.get(cv2.CAP_PROP_FPS)
-        num_frames_vid = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
-        ret = True
-        while ret:
-            ret, frame = video.read()
-        vid.release()
-
-        self.assertEqual(num_frames, num_frames_vid)
-        self.assertEqual(fps, fps_vid)
-        self.assertEqual(1600, width_vid)
-        self.assertEqual(600, height_vid)
-
+        # vs.make_video_from_videos(["./imgvid_utils/test_files/00000.MTS", "./imgvid_utils/test_files/00000.MTS"]
+        #                           , "temp/", "kitty", "mp4", cols=2, rows=1, width=800, height=600)
+        #
+        # video = cv2.VideoCapture("./imgvid_utils/test_files/00000.MTS")
+        # fps = video.get(cv2.CAP_PROP_FPS)
+        # num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+        # ret = True
+        # while ret:
+        #     ret, frame = video.read()
+        # video.release()
+        # vid = cv2.VideoCapture("./temp/kitty.mp4")
+        # width_vid = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # height_vid = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # fps_vid = vid.get(cv2.CAP_PROP_FPS)
+        # num_frames_vid = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        # ret = True
+        # while ret:
+        #     ret, frame = video.read()
+        # vid.release()
+        #
+        # self.assertEqual(num_frames, num_frames_vid)
+        # self.assertEqual(fps, fps_vid)
+        # self.assertEqual(1600, width_vid)
+        # self.assertEqual(600, height_vid)
+        pass
 
 if __name__ == '__main__':
     unittest.main()
