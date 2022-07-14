8cfaa63fa8a11868eee83d3a704e933020bd7bbd
philippeitis <33013301+philippeitis@users.noreply.github.com>
2022-07-13 19:53:46 -0700

    Improve image stacking code

--- imgvid_utils/imagestacker.py
+++ imgvid_utils/imagestacker.py
@@ -1,6 +1,6 @@
 import cv2
 from enum import Enum
-from typing import Union, List, Tuple
+from typing import Union, List, Tuple, Iterable
 import os
 import numpy as np
 
@@ -12,6 +12,16 @@ class Resize(Enum):
     DOWN = 4
     FIRST = 5
     NONE = 0
+
+    def filter(self):
+        return {
+            Resize.FIRST: return_first,
+            Resize.UP: return_max,
+            Resize.DOWN: return_min
+        }.get(self, return_first)
+
+    def choose(self, dims: Iterable[Tuple[int, int]]) -> Tuple[int, int]:
+        return self.filter()(dims)
 
     def __str__(self):
         return self.name.lower()
@@ -49,7 +59,7 @@ class ImageDataStore:
 
 class ImageGenerator:
     # TODO: allow multiple file extension inputs.
-    def __init__(self, directories, exts=None, num=1):
+    def __init__(self, directories, exts=None, stacking: Stacking = None):
         """
         Pass in paths to directories containing images, and extension
         of desired image inputs. Will return num images each iteration.
@@ -59,6 +69,8 @@ class ImageGenerator:
         :param exts:        Extension(s) of file to return
         :param num:         Number of images to return each iteration.
         """
+        self.stacking = stacking or Stacking.default()
+
         if isinstance(directories, str):
             self.directories = [directories]
         else:
@@ -66,11 +78,13 @@ class ImageGenerator:
                 raise ValueError("No directories provided.")
             self.directories = directories
 
-        if len(directories) % num != 0:
+        self.ext = exts or ["jpg"]
+        self.stacking = stacking or Stacking.default()
+        self.num = self.stacking.cols * self.stacking.rows
+
+        if len(directories) % self.num != 0:
             raise ValueError("Directories will not be sampled evenly.")
 
-        self.ext = exts or ["jpg"]
-        self.num = num
         self.files = {}
         self._max_iters = 0
         self._max_index = 0
@@ -83,6 +97,10 @@ class ImageGenerator:
             raise ValueError("One or more directories do not exist.")
 
         self._load_dirs()
+
+    @property
+    def dimensions(self):
+        return ()
 
     @property
     def max_iters(self):
@@ -266,7 +284,7 @@ def stack_images(images, stacking: Stacking):
     )
 
 
-def resize_images(images, dimensions: (int, int)):
+def resize_images(images, dimensions: Tuple[int, int]):
     """
     Resizes all of the images to the specified dimensions.
     :param images:          A set of opened images.
@@ -339,16 +357,9 @@ def make_images_from_folders_match(
 
     stacking = stacking or Stacking.default()
 
-    if resize_opt == Resize.UP:
-        fn = return_max
-    elif resize_opt == Resize.DOWN:
-        fn = return_min
-    else:
-        fn = return_first
-
     if size is None:
         def dims(images):
-            return fn([(image.shape[1], image.shape[0]) for image in images])
+            return resize_opt.choose([(image.shape[1], image.shape[0]) for image in images])
     else:
         def dims(_):
             return size
@@ -391,7 +402,6 @@ def make_images_from_folders(
 
     image_iter = ImageGenerator(dirs_in, exts=ext_in, num=stacking.cols * stacking.rows)
     image_iter.max_iters = image_iter.max_iters // (max_imgs if max_imgs else 1)
-
     os.makedirs(dir_out, exist_ok=True)
     num_zeros = len(str(image_iter.max_iters - 1))
 
@@ -429,19 +439,10 @@ def get_dimensions_files(
     if len(files_in) == 0:
         raise ValueError("get_dimensions_files requires at least one file.")
 
-    if resize == Resize.FIRST:
-        lmbda = return_first
-    elif resize == Resize.UP:
-        lmbda = return_max
-    elif resize == Resize.DOWN:
-        lmbda = return_min
-    else:
-        lmbda = return_first
-
     if not fo.has_video_exts(exts):
-        return lmbda(get_img_dimensions(files_in))
+        return resize.choose(get_img_dimensions(files_in))
     else:
-        return lmbda(get_video_dimensions(files_in))
+        return resize.choose(get_video_dimensions(files_in))
 
 
 def get_dimensions_dirs(dirs_in: Union[List[str], str], ext: str, resize: Resize):
@@ -458,27 +459,15 @@ def get_dimensions_dirs(dirs_in: Union[List[str], str], ext: str, resize: Resize
     if len(dirs_in) == 0:
         raise ValueError("Insufficient directories.")
 
-    if resize == Resize.FIRST:
-        lmbda = return_first
-    elif resize == Resize.UP:
-        lmbda = return_max
-    elif resize == Resize.DOWN:
-        lmbda = return_min
-    else:
-        lmbda = return_first
+    def dimension_generator():
+        for dir_in in dirs_in:
+            try:
+                yield get_dimensions_files(fo.get_files(dir_in, ext), ext, resize)
+            except ValueError:
+                continue
 
-    dims = []
-    for dir_in in dirs_in:
-        try:
-            dims.append(
-                get_dimensions_files(fo.get_files(dir_in, ext), ext, resize)
-            )
-            if lmbda == return_first:
-                return_first(dims)
-        except ValueError:
-            continue
     try:
-        return lmbda(dims)
+        return resize.choose(dimension_generator())
     except ValueError:
         if isinstance(ext, str):
             ext = [ext]
@@ -488,52 +477,46 @@ def get_dimensions_dirs(dirs_in: Union[List[str], str], ext: str, resize: Resize
         )
 
 
-def get_img_dimensions(imgs_in: List[str]):
+def get_img_dimensions(imgs_in: Iterable[str]):
     """
     Given a list of file paths, returns the dimensions of the images corresponding to the file paths
     :param imgs_in:     List of file paths corresponding to image files.
     :return:            List of corresponding file dimensions.
     """
-    dims_list = []
     for img in imgs_in:
         file = cv2.imread(img)
-        dims_list.append((file.shape[1], file.shape[0]))
-
-    return dims_list
+        yield file.shape[1], file.shape[0]
 
 
-def get_video_dimensions(videos_in: List[str]):
+def get_video_dimensions(videos_in: Iterable[str]):
     """
     Given a list of file paths, returns the dimensions of the videos corresponding to the file paths.
 
     :param videos_in:   List of file paths corresponding to video files.
     :return:            List of corresponding file dimensions.
     """
-    dims_list = []
-
     for vid in videos_in:
         file = cv2.VideoCapture(vid)
-        dims_list.append(
-            (
-                int(file.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                int(file.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-            )
-        )
+        yield (int(file.get(cv2.CAP_PROP_FRAME_WIDTH)),
+               int(file.get(cv2.CAP_PROP_FRAME_HEIGHT)))
         file.release()
 
-    return dims_list
 
-
-def return_first(arr: list):
+def return_first(items):
     """
     Returns the first element in an array.
-    :param arr:     Any list.
-    :return:        The first element in the list.
+    :param items:       Any iterable.
+    :return:            The first item in the iterable, or None if the iterable is empty.
     """
-    return arr[0] if len(arr) > 0 else None
+    try:
+        return next(items)
+    except TypeError:
+        return next(iter(items))
+    except StopIteration:
+        return None
 
 
-def return_max(list_of_dims):
+def return_max(list_of_dims: Iterable[Tuple[int, int]]) -> Tuple[int, int]:
     """
     Returns the dimensions that produce the maximum area.
     In the event of a tie, will return the first match.
@@ -541,10 +524,10 @@ def return_max(list_of_dims):
     :param list_of_dims: A list of dimensions.
     :return:             The dimensions with the greatest area.
     """
-    return max(list_of_dims, key=lambda dim: dim[0] * dim[1])
+    return max(list_of_dims, key=lambda dim: dim[0] * dim[1], default=None)
 
 
-def return_min(list_of_dims):
+def return_min(list_of_dims: Iterable[Tuple[int, int]]) -> Tuple[int, int]:
     """
     Returns the dimensions that produce the minimum area.
     In the event of a tie, will return the first match.
@@ -552,4 +535,4 @@ def return_min(list_of_dims):
     :param list_of_dims: A list of dimensions.
     :return:             The dimensions with the minimum area.
     """
-    return min(list_of_dims, key=lambda dim: dim[0] * dim[1])
+    return min(list_of_dims, key=lambda dim: dim[0] * dim[1], default=None)
